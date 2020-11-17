23e125e04247e4fd0a9676356bed294bad115db9
Dimitri Szygowski <dimitri.szygo@gmail.com>
2020-11-17 16:32:33 +0100

    Compare secret number to user input

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
+(function () {
     // your code here
 
 
-    document.getElementById("js-play").addEventListener("click", function(event) {
+    document.getElementById("js-play").addEventListener("click", function (event) {
 
         event.preventDefault();
 
-            function generateSecretNumber () {
-                const randomNumber = Math.floor(Math.random() * 22) + 1;
-                return randomNumber;
-            }
+        function generateSecretNumber() {
+            return Math.floor(Math.random() * 22) + 1;
+        }
+
+        function userInput() {
+            return Number(document.getElementById("js-user-guess").value);
+        }
 
         const secretNumber = generateSecretNumber();
+        const userGuess = userInput();
 
 
-        const userGuess = Number(document.getElementById("js-user-guess").value);
-        console.log(userGuess);
-
-        if (secretNumber === userGuess){
+        if (secretNumber === userGuess) {
             window.alert("Awesome! You number " + userGuess + " was correct. You can be named many things, hungry not being one of them.");
-        } else if (secretNumber == userGuess + 1 || secretNumber == userGuess - 1) {
+        } else if (secretNumber === userGuess + 1 || secretNumber === userGuess - 1) {
             window.alert("So close, but you just missed it! Are you in a class that started on the thirteenth or what?");
-        }
-        else {
+        } else {
             window.alert("Bummer... You guessed " + userGuess + " and the secret number was " + secretNumber + ".");
         }
     });
